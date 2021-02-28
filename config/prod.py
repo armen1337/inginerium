3437d65de8be03a3f363622ac0b01416a1a14387
Armen <armen14102003@mail.ru>
2021-02-28 19:28:10 +0400

    Preparing for deployment

--- config/prod.py
+++ config/prod.py
@@ -4,22 +4,22 @@ from .base import *
 DEBUG = False
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql',
-		'NAME': 'inginerium_databases',
-		'USER': 'postgres',
-		'PASSWORD': '123456',
-		'HOST': 'localhost',
-		'PORT': '5432',
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.postgresql',
+# 		'NAME': 'inginerium_databases',
+# 		'USER': 'postgres',
+# 		'PASSWORD': '123456',
+# 		'HOST': 'localhost',
+# 		'PORT': '5432',
+# 	}
+# }
