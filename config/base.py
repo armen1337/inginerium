c89e6571decc82179cf5899d7f1d60edac505b35
Armen <armen14102003@mail.ru>
2021-03-07 19:15:27 +0400

    Some changes

--- config/base.py
+++ config/base.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-CSRF_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
 
